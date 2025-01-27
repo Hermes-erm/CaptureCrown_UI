ce72c030caad49345e204755948266b654fb6f32
sudhakaran-bt <Sudhagaran.Rajendran@motherson.com>
2025-01-27 17:21:22 +0530

    updated code

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { DragControls } from 'three/examples/jsm/controls/DragControls.js';
 // import { OrbitControls } from 'three-orbitcontrols-ts';
 
 @Component({
@@ -16,20 +17,27 @@ export class HomeComponent {
   scene = new THREE.Scene();
   camera: THREE.PerspectiveCamera | null = null; // better perspective camera over orthographic camera
 
+  planeColor: number = 0xffffff; //  or THREE.ColorRepresentation (type)
+  boxColor: number = 0x3256a8;
+
   orbit: OrbitControls | null = null;
 
-  boxGeometry = new THREE.BoxGeometry();
-  boxMaterial = new THREE.MeshBasicMaterial({ color: 0xff3335 });
-  box = new THREE.Mesh(this.boxGeometry, this.boxMaterial);
+  box: THREE.Mesh | null = null;
 
-  planeGeometry = new THREE.PlaneGeometry(10, 10);
-  planeMaterial = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    side: THREE.DoubleSide,
-  });
-  plane = new THREE.Mesh(this.planeGeometry, this.planeMaterial);
+  planeBox: THREE.Mesh | null = null;
 
   gridHelper = new THREE.GridHelper(10, 10);
+
+  mousePosition = new THREE.Vector2();
+  mouseMove = new THREE.Vector2();
+
+  rayCaster = new THREE.Raycaster();
+
+  intersects: any | null = null;
+
+  draggable: THREE.Object3D | null = null; // manipulate object in 3d space
+
+  dragControl: DragControls | null = null;
 
   ngOnInit() {
     this.container = document.getElementById('playground'); // element where we gonna put renderer
@@ -39,14 +47,14 @@ export class HomeComponent {
       this.container?.clientWidth,
       this.container?.clientHeight
     );
+
     let axesHelper = new THREE.AxesHelper(5); // just to guide us with axes
 
-    this.box.position.set(0, 0.5, 0);
+    this.createBox();
+    this.createFloor();
 
-    this.scene.add(axesHelper, this.box, this.plane);
+    this.scene.add(axesHelper);
     this.scene.add(this.gridHelper);
-
-    this.plane.rotation.x = -Math.PI / 2; // in radian
 
     this.camera = new THREE.PerspectiveCamera(
       75, // fov, camera angle of view ( that cover )
@@ -55,31 +63,74 @@ export class HomeComponent {
       500 // far clipping plane
     );
 
-    this.camera.position.set(2, 2, 2); // x, y, z
+    this.camera.position.set(5, 5, 5); // x, y, z
+    this.camera.lookAt(0, 0, 0);
 
     // // now render the scene
     this.orbit = new OrbitControls(this.camera, this.renderer.domElement); // Also can it refered as => which navigate the scene by angular view
 
     // this.orbit.enableDamping = true;
+
     this.orbit.enableZoom = true;
     this.orbit.zoomSpeed = 1;
     this.orbit.enablePan = true;
-    this.orbit.enabled = true;
+    this.orbit.enableRotate = false; // true to enable orbiting..
 
     // this.renderer.render(this.scene, this.camera); // only next to orbit control, cz then we can also access orbit in the scene and via camera
 
+    if (this.box)
+      this.dragControl = new DragControls(
+        [this.box],
+        this.camera,
+        this.renderer.domElement
+      );
+
     this.animate();
+    this.listenEvent();
   }
 
   animate() {
     requestAnimationFrame(() => this.animate());
-    this.orbit?.update();
+    // this.orbit?.update();
     if (this.camera) this.renderer.render(this.scene, this.camera);
-    // this.container?.addEventListener('mousemove', () => {
-    //   if (!this.camera || !this.orbit) return;
-    //   this.orbit.update();
-    //   this.renderer.render(this.scene, this.camera);
-    //   console.log('hey');
-    // });
+  }
+
+  listenEvent() {
+    if (!this.dragControl) return;
+    this.dragControl.addEventListener('drag', (e) => {
+      if (!this.orbit) return;
+      // this.orbit.enableZoom = false;
+      // this.orbit.zoomSpeed = 1;
+      // this.orbit.enablePan = false;
+      // this.orbit.enabled = false;
+      e.object.position.y = 0.5;
+      // if (this.camera) this.renderer.render(this.scene, this.camera);
+    });
+  }
+
+  createFloor() {
+    let depth = 0.2;
+    let planeBoxGeometry = new THREE.BoxGeometry(10, 10, depth);
+    let planeBoxMaterial = new THREE.MeshBasicMaterial({
+      color: this.planeColor,
+      side: THREE.DoubleSide,
+    });
+    this.planeBox = new THREE.Mesh(planeBoxGeometry, planeBoxMaterial);
+
+    this.planeBox.rotation.x = -Math.PI / 2;
+    this.planeBox.position.y = -(depth / 2); // half of depth
+
+    this.scene.add(this.planeBox);
+    this.planeBox.userData = { ground: true };
+  }
+
+  createBox() {
+    let boxGeometry = new THREE.BoxGeometry();
+    let boxMaterial = new THREE.MeshBasicMaterial({ color: this.boxColor });
+    this.box = new THREE.Mesh(boxGeometry, boxMaterial);
+    this.box.position.set(0, 0.5, 0);
+    this.scene.add(this.box);
+
+    this.box.userData = { draggable: false, name: 'BOX' }; // or this.box.userData['draggable'] = true;
   }
 }
